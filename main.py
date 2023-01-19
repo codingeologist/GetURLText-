f80afa62846f42408628fed15b09f9d7bbf6a9ff
Coding Geologist <51208321+codingeologist@users.noreply.github.com>
2023-01-19 18:22:24 +0000

    new

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ import requests
 import PySimpleGUI as sg
 from bs4 import BeautifulSoup as bs
 
+
 def get_text(x):
     html = requests.get(x).content
-    
+
     unicode_str = html.decode('utf8')
-    encoded_str = unicode_str.encode('ascii','ignore')
-    soup = bs(encoded_str,'html.parser')
-    
+    encoded_str = unicode_str.encode('ascii', 'ignore')
+    soup = bs(encoded_str, 'html.parser')
+
     element = soup.find_all('p')
-    text_list=[re.sub(r'<.+?>',r'',str(p)) for p in element]
-    
+    text_list = [re.sub(r'<.+?>', r'', str(p)) for p in element]
+
     text = ''
     for line in text_list:
-        text += '{}'.format(line,'\n')
-    
+        text += '{}'.format(line, '\n')
+
     return text
+
 
 if __name__ == '__main__':
     sg.theme('DarkGrey2')
@@ -26,7 +28,7 @@ if __name__ == '__main__':
 
     window = sg.Window('Get URL Text!', layout, finalize=True)
     window.Maximize()
-    
+
     while True:
         event, values = window.Read()
         if event in (sg.WIN_CLOSED, 'Cancel'):
